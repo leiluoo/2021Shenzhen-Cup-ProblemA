cce520f3c2233a159b98a32c4c7004c0a0abd8b3
ArthurOyll <88656452+ArthurOyll@users.noreply.github.com>
2021-09-01 22:01:37 +0800

    Update test.py
    edit some details

--- sheepAndDog/test.py
+++ sheepAndDog/test.py
@@ -1,9 +1,17 @@
 import time
+import random
+from math import pi
 
 import gym
 
 env = gym.make('Farm_Env-v0')
-env.reset()
-env.render()
+action = random.uniform(0, pi)
 
-time.sleep(5)
+env.reset()
+
+for i in range(500):
+    env.step(env.action_space.sample())
+    env.render()
+    time.sleep(0.1)
+
+env.close()
